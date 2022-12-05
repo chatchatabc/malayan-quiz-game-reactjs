1bf45952fde95c29e7fecabc47a6b38bef56a835
BoJoNVi <bonjomontes@gmail.com>
2022-12-05 10:29:51 +0800

    Implemented winners page

--- src/components/quiz game/WinnersDisplay.tsx
+++ src/components/quiz game/WinnersDisplay.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import MotionDiv from "../widgets/MotionDiv";
 
 function WinnersDisplay() {
-  return <div>WinnersDisplay</div>;
+  return (
+    <MotionDiv className="flex-1 p-2 relative">
+      <div className="flex flex-col items-center">
+        <h2 className="text-5xl font-extrabold py-8">Winners</h2>
+        <div className="w-fit text-center">
+          {[...Array(10)].map((x, index) => {
+            return <p className="p-5 text-2xl">Bon Jovi Montes {index}</p>;
+          })}
+        </div>
+      </div>
+      <div className="absolute translate-x-1/2 inset-0 mix-blend-multiply">
+        <div className="firework"></div>
+      </div>
+      <div className="absolute -translate-x-1/2 inset-0 mix-blend-multiply">
+        <div className="firework"></div>
+      </div>
+    </MotionDiv>
+  );
 }
 
 export default WinnersDisplay;
